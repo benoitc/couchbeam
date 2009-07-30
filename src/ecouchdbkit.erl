9d597551e36cc64c81387ac09a66edd75a3fe983
Benoit Chesneau <bchesneau@gmail.com>
2009-07-30 08:20:19 +0200

    first bits of documentation

--- src/ecouchdbkit.erl
+++ src/ecouchdbkit.erl
@@ -29,11 +29,22 @@
 -export([server_info/1, all_dbs/1, db_info/2, create_db/2, delete_db/2,
          uuids/0, uuids/1, uuids/2, next_uuid/0, next_uuid/1, open_doc/3, open_doc/4, 
          save_doc/3, save_doc/4, save_docs/3, save_docs/4, delete_doc/3, 
-         query_view/4, query_view/5, query_view/6, is_db/2, all_docs/3]).
+         query_view/4, query_view/5, query_view/6, is_db/2, 
+         all_docs/3, all_docs_by_seq/3]).
 
          
 -include("ecouchdbkit.hrl").
 -define(SERVER, ?MODULE).
+
+%% @type node_info() = {Host::string(), Port::int()}
+%% @type iolist() = [char() | binary() | iolist()]
+%% @type iodata() = iolist() | binary()
+%% @type json_string() = atom | binary()
+%% @type json_number() = integer() | float()
+%% @type json_array() = [json_term()]
+%% @type json_object() = {struct, [{json_string(), json_term()}]}
+%% @type json_term() = json_string() | json_number() | json_array() |
+%%                     json_object()
 
 
 start() ->
@@ -51,12 +62,23 @@ stop(_Reason) ->
 sup_start_link() ->
     gen_server:start_link({local, ecouchdbkit}, ecouchdbkit, [], []).
     
+    
+%% @spec open_connection(node()) -> ok
+%% @type node() = {NodeName::atom()|string(), Hostname::string(), Port::integer()}
+%% @doc When you use ecouchdbkit as an application (OTP mode) you could setup the 
+%% connection and open a gen_server that will be supervised. It allow you
+%% for now to set once the CouchDB node and reuse settings. In a future version it will
+%% be possible to setup loadbalancing and such things.
 open_connection({NodeName, {Host, Port}}) ->
     gen_server:call(ecouchdbkit, {open_connection, {NodeName, {Host, Port}}}).
     
+%% @spec json_encode(V::json_term()) -> iolist()
+%% @doc Encode to json
 json_encode(V) ->
     ecouchdbkit_mochijson2:encode(V).
     
+%% @spec json_decode(V::iolist()) -> json_term()
+%% @doc decode from json string
 json_decode(V) ->
     ecouchdbkit_mochijson2:decode(V).
     
@@ -96,32 +118,50 @@ next_uuid(NodeName) ->
         next_uuid(NodeName)
     end.
     
+    
+%% @spec all_dbs(NodenName::atom()|node_info()) -> list()
+%% @doc fetch list of all dbs
 all_dbs(NodeName) ->
     Resp = make_request(NodeName, 'GET', "/_all_dbs", []),
     do_reply(Resp).
     
+    
 %% db operations   
     
+%% @spec db_info(NodeName::atom()|node_info(), DbName::string()) -> list()
+%% @doc fetch information of Database
 db_info(NodeName, DbName) ->
     Resp = make_request(NodeName, 'GET', "/" ++ DbName, []),
     do_reply(Resp).
- 
+    
+    
+%% @spec create_db(NodeName::atom()|node_info(), DbName::string()) -> ok
+%% @doc create a database with DbName
 create_db(NodeName, DbName) ->
     Resp = make_request(NodeName, 'PUT', "/" ++ DbName, []),
     do_reply(Resp).
     
+%% @spec delete_db(NodeName::atom()|node_info(), DbName::string()) -> ok
+%% @doc delete a database with dbname
 delete_db(NodeName, DbName) ->
     Resp = make_request(NodeName, 'DELETE', "/" ++ DbName, []),
     do_reply(Resp).
-    
+  
+%% @spec is_db(NodeName::atom()|node_info(), DbName::string()) -> true|false
+%% @doc If database exist in the node returns true
 is_db(NodeName, DbName) ->
     AllDbs = all_dbs(NodeName),
     lists:member(?l2b(DbName), AllDbs).
 
 %% document operations
 
+%% @spec open_doc(NodeName::atom()|node_info(), DbName::string(), DocId::string()) -> json_object()
+%% @doc open a doc with DocID
 open_doc(NodeName, DbName, DocId) ->
     open_doc(NodeName, DbName, DocId, nil).
+    
+%% @spec open_doc(NodeName::atom()|node_info(), DbName::string(), DocId::string(), Rev::string()) -> json_object()
+%% @doc open a doc with DocId for specific revision
 open_doc(NodeName, DbName, DocId, Rev) ->
     Path = "/" ++ DbName ++ "/" ++ DocId,
     Resp = case Rev of
@@ -132,6 +172,9 @@ open_doc(NodeName, DbName, DocId, Rev) ->
         end,
     do_reply(Resp).
     
+%% @spec save_doc(NodeName::atom()|node_info(), DbName::string(), Doc::json_object()) -> json_object() 
+%% @doc save a doc. If Id don't exist it will be created, 
+%% If _id and _rev are in Doc object, document will be updated
 save_doc(NodeName, DbName, Doc) ->
     {Props} = Doc,
     DocId = case proplists:get_value(<<"_id">>, Props) of
@@ -144,15 +187,21 @@ save_doc(NodeName, DbName, Doc) ->
     end,
     save_doc(NodeName, DbName, DocId, Doc).
     
+%% @spec save_doc(NodeName::atom()|node_info(), DbName::string(), Doc::json_object(), DocId::string) -> json_object() 
+%% @doc save a do with DocId. 
 save_doc(NodeName, DbName, DocId, Doc) ->
     Path = "/" ++ DbName ++ "/" ++ encode_docid(DocId),
     Body = ecouchdbkit:json_encode(Doc),
     Resp = make_request(NodeName, 'PUT', Path, Body, [], []),
     do_reply(Resp).
     
+%% @spec save_docs(NodeName::atom()|node_info(), DbName::string(), Docs::json_array()) -> json_object() 
+%% @doc bulk update
 save_docs(NodeName, DbName, Docs) ->
     save_docs(NodeName, DbName, Docs, []).
-        
+  
+%% @spec save_docs(NodeName::atom()|node_info(), DbName::string(), Docs::json_array(), opts: lists()) -> json_object() 
+%% @doc bulk update with options, currently support only all_or_nothing.     
 save_docs(NodeName, DbName, Docs, Opts) ->
     Docs1 = [maybe_docid(NodeName, Doc) || Doc <- Docs],
     JsonObj = case proplists:get_value(all_or_nothing, Opts, false) of
@@ -165,40 +214,56 @@ save_docs(NodeName, DbName, Docs, Opts) ->
     do_reply(Resp).
     
 
+%% @spec delete_doc(NodeName::atom()|node_info(), DbName::string(), Doc::json_object()) -> json_object() 
+%% @doc delete a document
 delete_doc(NodeName, DbName, {DocProps}) ->
     Doc1 = {[{<<"_deleted">>, true}|DocProps]},
     save_doc(NodeName, DbName, Doc1).
 
+%% @spec all_docs(NodeName::atom()|node_info(), DbName::string(), Params::list()) -> json_object()
+%% @doc This method has the same behavior as a view. Return all docs
 all_docs(NodeName, DbName, Params) ->
     Path = io_lib:format("/~s/_all_docs", [DbName]),
     fetch_view(NodeName, Path, Params).
     
+%% @spec all_docs_by_seq(NodeName::atom()|node_info(), DbName::string(), Params::list()) -> json_object()
+%% @doc This method has the same behavior as a view. 
+%% Return an updated list of all documents.
+all_docs_by_seq(NodeName, DbName, Params) ->
+    Path = io_lib:format("/~s/_all_docs_by_seq", [DbName]),
+    fetch_view(NodeName, Path, Params).
+    
+
+%% @spec query_view(NodeName::atom()|node_info(), DbName::string(), 
+%%                      DName::design_name(),ViewName::view_name()) -> json_object()
+%% @type design_name() = string()
+%% @type view_name() = string()
+%% @doc query a view and return all results
 query_view(NodeName, DbName, DName, ViewName) ->
     query_view(NodeName, DbName, DName, ViewName, []).
-    
+
+%% @spec query_view(NodeName::atom()|node_info(), DbName::string(), 
+%%                  DName::design_name(),ViewName::view_name(),
+%%                  Params::view_params()) -> json_object()
+%% @type view_params() = proplist()
+%% @doc query a view and return results depending on params   
 query_view(NodeName, DbName, DName, ViewName, Params) ->
     Path = io_lib:format("/~s/_design/~s/_view/~s", [DbName, DName, ViewName]),
     fetch_view(NodeName, Path, Params).
-    
+
+%% @spec query_view(NodeName::atom()|node_info(), DbName::string(), 
+%%                  DName::design_name(),ViewName::view_name(),
+%%                  Params::view_params(), Fun::view_fun()) -> json_object()
+%% @type view_fun() = fun_arity_0() | {fun_arity_1(), initial_state()} 
+%% @doc query a view and return results depending on params, fun could be specfied
+%% to pare the incoming view. See parse_incoming_view in examples.
+%% 
 query_view(NodeName, DbName, DName, ViewName, Params, Fun) ->
     Path = io_lib:format("/~s/_design/~s/_view/~s", [DbName, DName, ViewName]),
     fetch_view(NodeName, Path, Params, Fun).
     
-fetch_view(NodeName, Path, Params) ->
-    fetch_view(NodeName, Path, Params, fun ecouchdbkit_client:body_fun/2).
-    
-fetch_view(NodeName, Path, Params, Fun) ->
-    Resp = case proplists:get_value("keys", Params) of
-        undefined -> 
-            make_request(NodeName, 'GET', Path, nil, [], Params, Fun);
-        Keys ->
-            Params1 = proplists:delete("keys", Params),
-            Body = ecouchdbkit:json_encode({struct, {<<"keys">>, Keys}}),
-            make_request(NodeName, 'POST', Path, Body, [], Params1, Fun)
-        end,
-    do_reply(Resp).
-    
-    
+%% @spec do_reply(Resp::term()) -> term()
+%% @doc make transformation on final http response
 do_reply(Resp) ->
     case Resp of
     {error, Reason} -> throw(Reason);
@@ -208,6 +273,10 @@ do_reply(Resp) ->
     Other -> Other
     end.
     
+%% @spec make_request(NodeName::atom()|node_info(),Method::string(), 
+%%                  Path::string(),Headers::headers()) -> term()
+%% @type headers() = [tuple()]
+%% @doc function used to create and send request.
 make_request(NodeName, Method, Path, Headers) ->
     make_request(NodeName, Method, Path, nil, Headers, []).
     
@@ -236,6 +305,7 @@ make_request(NodeName, Method, Path, Body, Headers, Params, Fun) ->
         gen_server:call(Pid, {request, Method, Path, Body, Headers, Params, Fun})
     end.    
     
+%% gen servers calls  
 init([]) ->
     Tid = ets:new(ecouchdbkit_uuids, [public, ordered_set]),
     State = #ecouchdbkit_srv{
@@ -301,6 +371,25 @@ handle_info(Info, _Server) ->
     exit({unknown_message, Info}).
     
 %% Internal API
+
+%% @private
+%% @doc function used to fetch view
+fetch_view(NodeName, Path, Params) ->
+    fetch_view(NodeName, Path, Params, fun ecouchdbkit_client:body_fun/2).
+%% @private    
+fetch_view(NodeName, Path, Params, Fun) ->
+    Resp = case proplists:get_value("keys", Params) of
+        undefined -> 
+            make_request(NodeName, 'GET', Path, nil, [], Params, Fun);
+        Keys ->
+            Params1 = proplists:delete("keys", Params),
+            Body = ecouchdbkit:json_encode({struct, {<<"keys">>, Keys}}),
+            make_request(NodeName, 'POST', Path, Body, [], Params1, Fun)
+        end,
+    do_reply(Resp).
+
+%% @private
+%% Create a ecouchdbkit_client sertver for a nodename if it don't exist
 open_connection1({NodeName, {Host, Port}}, Nodes) ->
     NodeName1 = nodename(NodeName),
     Client = {NodeName1,
