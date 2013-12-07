a305f017b2114200738a08befeddeb7ce0bfa88d
benoitc <bchesneau@gmail.com>
2013-12-07 16:44:19 +0100

    quick fix release. bump to 0.9.3
    fixing hackney

--- package.exs
+++ package.exs
@@ -1,5 +1,5 @@
 Expm.Package.new(name: "couchbeam", description: "Apache CouchDB client in Erlang",
-                 version: "0.9.2", keywords: ["couchdb",
+                 version: "0.9.3", keywords: ["couchdb",
                                               "http","client","binary",
                                               "database"],
                  dependencies: ["hackney"],
@@ -7,5 +7,5 @@ Expm.Package.new(name: "couchbeam", description: "Apache CouchDB client in Erlan
                  contributors: [],
                  maintainers: [[name: "Benoit Chesneau",
                                 email: "bchesneau@gmail.com"]],
-                 repositories: [[github: "benoitc/couchbeam", tag: "0.9.2"]])
+                 repositories: [[github: "benoitc/couchbeam", tag: "0.9.3"]])
 
