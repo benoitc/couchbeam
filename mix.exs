d68339fa17a5803c6e194d6de99b1693e69d4035
Valentin Tsatskin <val@valtsatskin.com>
2017-02-08 11:00:52 -0500

    Revert "remove mix build"
    This reverts commit d7c3f476287467291c065d3ba4c58bdf8d990923.

---
+++ mix.exs
@@ -0,0 +1,60 @@
+defmodule Couchbeam.Mixfile do
+use Mix.Project
+
+    def project do
+        [
+            app: :couchbeam,
+            version: "1.4.0",
+            description: "Erlang CouchDB client",
+            deps: deps,
+            package: package,
+            language: :erlang
+        ]
+    end
+
+    def application do
+        [
+            applications:
+                [
+                    :kernel,
+                    :stdlib,
+                    :crypto,
+                    :asn1,
+                    :public_key,
+                    :ssl,
+                    :hackney
+                ],
+            env:
+                [
+                ],
+            mod: {:couchbeam_app, []}
+        ]
+    end
+
+    def deps do
+        [
+            {:hackney, "~> 1.6.2"},
+            {:jsx, "~> 2.8.0"}
+        ]
+    end
+
+    defp package do
+        [
+            files: [
+                "src",
+                "include",
+                "mix.exs",
+                "mix.lock",
+                "rebar.config",
+                "rebar.lock",
+                "README.md",
+                "NEWS.md",
+                "LICENSE",
+                "NOTICE"
+            ],
+            maintainers: ["Benoit Chesneau"],
+            licenses: ["MIT"],
+            links: %{"Github" => "https://github.com/benoitc/couchbeam"}
+        ]
+    end
+end
