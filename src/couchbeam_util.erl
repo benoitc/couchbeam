2160df7baf6230c9631c8aab3b58bf258de7dfe7
PatrickSachs <kontakt@patrick-sachs.de>
2021-11-03 11:52:48 +0100

    Updated crypto function to OTP 24

--- src/couchbeam_util.erl
+++ src/couchbeam_util.erl
@@ -280,11 +280,4 @@ proxy_token(Secret,UserName) ->
     hackney_bstr:to_hex(hmac(sha, Secret, UserName)).
 
 hmac(Alg, Key, Data) ->
-    case {Alg, erlang:function_exported(crypto, hmac, 3)} of
-        {_, true} ->
-            crypto:hmac(Alg, Key, Data);
-        {sha, false} ->
-            crypto:sha_mac(Key, Data);
-        {Alg, false} ->
-            throw({unsupported, Alg})
-    end.
+    crypto:mac(hmac, Alg, Key, Data).
